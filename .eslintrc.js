60ce5d4cd0c9fec30e9cf73909ad9a025b2968bc
Qiulin <110177673+RS-Imagine@users.noreply.github.com>
2023-11-30 09:26:49 +0800

    Add files via upload

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@next/next/recommended',
-    'standard',
-	'next'
+    'standard'
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -31,9 +30,7 @@ module.exports = {
     'react/no-unknown-property': 'off', // <style jsx>
     'react/prop-types': 'off',
     'space-before-function-paren': 0,
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-	'react/no-unescaped-entities': 'off',
-    '@next/next/no-page-custom-font': 'off'
+    'react-hooks/rules-of-hooks': 'error' // Checks rules of Hooks
   },
   globals: {
     React: true

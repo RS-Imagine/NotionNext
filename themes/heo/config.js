d0b45d08c5bfd5a9b8fd4c22ab2b8e52b42d24a7
Qiulin <110177673+RS-Imagine@users.noreply.github.com>
2023-12-04 23:18:36 +0800

    updata heo_confiig.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,15 +6,15 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://forimagine.eu.org' },
-    { title: '猥以蒙鄙之姿，得觀希世之寶', url:'' }
+    { title: '猥以蒙鄙之姿，得觀希世之寶' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '以蒙鄙之姿',
-  HEO_HERO_TITLE_2: '觀希世之寶',
+  HEO_HERO_TITLE_1: '猥以蒙鄙之姿',
+  HEO_HERO_TITLE_2: '得觀希世之寶',
   HEO_HERO_TITLE_3: 'forimagine.eu.org',
   HEO_HERO_TITLE_4: '个人博客',
-  HEO_HERO_TITLE_5: 'Blog For Imagine|Sharing with you',
+  HEO_HERO_TITLE_5: 'Blog For Imagine | Sharing with you',
   HEO_HERO_TITLE_LINK: 'https://forimagine.eu.org',
 
   // 英雄区显示三个置顶分类
@@ -24,18 +24,17 @@ const CONFIG = {
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
-  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://pixabay.com/photos/coffee-pen-notebook-open-notebook-2306471/', // 英雄区右侧图片
+  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    '你好！',
     '🔍 主修遥感科学技术',
-    '',
     '🏃 在此分享技术经验',
     '👋 欢迎来到我的博客'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/RS-Imagine', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -47,6 +46,54 @@ const CONFIG = {
       img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
       color_2: '#ffffff'
     },
+    {
+      title_1: 'Docker',
+      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
+      color_1: '#57b6e6',
+      title_2: 'Photoshop',
+      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
+      color_2: '#4082c3'
+    },
+    {
+      title_1: 'FinalCutPro',
+      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      color_1: '#ffffff',
+      title_2: 'Python',
+      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'Swift',
+      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
+      color_1: '#eb6840',
+      title_2: 'Principle',
+      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
+      color_2: '#8f55ba'
+    },
+    {
+      title_1: 'illustrator',
+      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
+      color_1: '#f29e39',
+      title_2: 'CSS3',
+      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
+      color_2: '#2c51db'
+    },
+    {
+      title_1: 'JS',
+      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
+      color_1: '#f7cb4f',
+      title_2: 'HTML',
+      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
+      color_2: '#e9572b'
+    },
+    {
+      title_1: 'Git',
+      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
+      color_1: '#df5b40',
+      title_2: 'Rhino',
+      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
+      color_2: '#1f1f1f'
+    }
   ],
 
   HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
